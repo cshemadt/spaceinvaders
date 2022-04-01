8dbbaf0d3d0c5442bf953faec46213155088b805
tinydumb <adustoevsky@gmail.com>
2022-04-01 12:33:12 +0300

    Temp commit

--- ship.cpp
+++ ship.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 
 //---------------------------- CONSTRUCTORS ----------------------------
+
 Ship::Ship(sf::Vector2u windowSize) : m_windowSize{windowSize} {
     spriteInit();
     reset();
 }
+
 //---------------------------- INITIALIZING METHODS ----------------------------
+
 void Ship::reset() {
     m_hp = 3;
     m_isAlive = true;
@@ -17,12 +20,14 @@ void Ship::reset() {
     setPosition(m_windowSize.x/2.0f, m_windowSize.y-50);
 }
 void Ship::spriteInit() {
-    m_shipTexture.loadFromFile("../src/ship.png");
+    m_shipTexture.loadFromFile("../src/enemy4_2x_2.png");
     m_sprite.setTexture(m_shipTexture);
-    m_sprite.setTextureRect(sf::IntRect(sf::Vector2i(0.f, 0.f), sf::Vector2i(200,200)));
+    m_sprite.setTextureRect(sf::IntRect(sf::Vector2i(0.f, 0.f), sf::Vector2i(64,64)));
     m_sprite.setOrigin(m_shipTexture.getSize().x/2.0f, m_shipTexture.getSize().y/2.0f);
 }
+
 //---------------------------- MAIN METHODS ----------------------------
+
 void Ship::render(sf::RenderWindow &renderWindow) { renderWindow.draw(m_sprite); }
 void Ship::move(const Direction &direction) {
     switch(direction){
@@ -38,11 +43,13 @@ void Ship::move(const Direction &direction) {
 
     }
 }
+
 //---------------------------- GETTERS AND SETTERS ----------------------------
 void Ship::setPosition(sf::Vector2f &position) {
     m_position=position;
     m_sprite.setPosition(position.x,position.y);
 }
+
 void Ship::setPosition(float x, float y) {
     m_position=sf::Vector2f{x,y};
     m_sprite.setPosition(x,y);
